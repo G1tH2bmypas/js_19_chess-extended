17d6ef113c731d03dec77d311b32d7e4617c98e1
Tekyto <Tekyto2014@gmail.com>
2022-11-16 20:38:42 +0200

    js_19_chess-extented+

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function chess(name, x1, y1, x2, y2) {
-	// write your code here
+	return name === 'pawn' ? x2 === x1 + 1 && y2 === y1 + 1 || x2 === x1 - 1 && y2 === y1 + 1 : name === 'rook' ? x2 === x1 || y2 === y1 : name === 'bishop' ? Math.abs(x2 - x1) === Math.abs(y2 - y1) : name === 'queen' ? x1 === x2 || y2 === y1 || Math.abs(y2 - y1) === Math.abs(x2 - x1) : name === 'king' ? Math.abs(x2 - x1) <= 1 && Math.abs(y1 - y2) <= 1 : name === 'knight' ? Math.abs(x2 - x1) === 2 && Math.abs(y2 - y1) === 1 || Math.abs(x2 - x1) === 1 && Math.abs(y2 - y1) === 2 : false;
 }
 
 module.exports = chess;
