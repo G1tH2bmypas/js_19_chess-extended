f6e6660534f9b27d4449fff27d4da2122e1c13fb
Tekyto <Tekyto2014@gmail.com>
2022-11-17 12:40:37 +0200

    js_19_chess-extended

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function chess(name, x1, y1, x2, y2) {
-	return name === 'pawn' ? x2 === x1 + 1 && y2 === y1 + 1 || x2 === x1 - 1 && y2 === y1 + 1 : name === 'rook' ? x2 === x1 || y2 === y1 : name === 'bishop' ? Math.abs(x2 - x1) === Math.abs(y2 - y1) : name === 'queen' ? x1 === x2 || y2 === y1 || Math.abs(y2 - y1) === Math.abs(x2 - x1) : name === 'king' ? Math.abs(x2 - x1) <= 1 && Math.abs(y1 - y2) <= 1 : name === 'knight' ? Math.abs(x2 - x1) === 2 && Math.abs(y2 - y1) === 1 || Math.abs(x2 - x1) === 1 && Math.abs(y2 - y1) === 2 : false;
+	if (name === 'pawn') {
+		return x2 === x1 + 1 && y2 === y1 + 1;
+	} else if (name === 'rook') {
+		return x2 === x1 || y2 === y1;
+	} else if (name === 'bishop') {
+		return Math.abs(x2 - x1) === Math.abs(y2 - y1);
+	} else if (name === 'queen') {
+		return x1 === x2 || y2 === y1 || Math.abs(y2 - y1) === Math.abs(x2 - x1);
+	} else if (name === 'king') {
+		return Math.abs(x2 - x1) <= 1 && Math.abs(y1 - y2) <= 1;
+	} else if (name === 'knight') {
+		return Math.abs(x2 - x1) === 2 && Math.abs(y2 - y1) === 1 || Math.abs(x2 - x1) === 1 && Math.abs(y2 - y1) === 2;
+	} else {
+		return false;
+	}
 }
 
 module.exports = chess;
